9eb396eb4387ad88ba589452dec8d68744e439db
Antonio Junior <antonio.junior@outlook.com>
2022-05-16 20:10:06 -0300

    Working whit four verbs

---
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNET5Udemy/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace RestWithASPNET5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Addres { get; set; }
+        public string Gender { get; set; }
+        public Person()
+        {
+        }
+    }
+}
